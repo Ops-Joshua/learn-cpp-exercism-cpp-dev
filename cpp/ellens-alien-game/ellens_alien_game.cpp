0036cbda6e967d9907b6fa715ee989420ec3a156
Joshua Tan <joshuatanir@gmail.com>
2026-07-27 11:30:33 +0800

    sln: ellen's alien game
    topic:
    - class

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -1,5 +1,52 @@
-namespace targets {
-// TODO: Insert the code for the alien class here
+namespace targets
+{
+    class Alien
+    {
+    private:
+        int health{3};
 
-}  // namespace targets
+    public:
+        Alien(int x, int y)
+        {
+            x_coordinate = x;
+            y_coordinate = y;
+        }
 
+        int get_health()
+        {
+            return health;
+        }
+
+        bool hit()
+        {
+            if (health > 0)
+            {
+                health -= 1;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        bool is_alive()
+        {
+            return (0 < health);
+        }
+
+        bool teleport(int x_new, int y_new)
+        {
+            x_coordinate = x_new;
+            y_coordinate = y_new;
+            return true;
+        }
+
+        bool collision_detection( Alien the_alien)
+        {
+            return ((the_alien.x_coordinate == x_coordinate)
+                && (the_alien.y_coordinate == y_coordinate));
+        }
+
+        int x_coordinate;
+        int y_coordinate;
+    };
+}
--- cpp/ellens-alien-game/ellens_alien_game_test.cpp
+++ cpp/ellens-alien-game/ellens_alien_game_test.cpp
@@ -13,7 +13,7 @@ TEST_CASE("Alien has correct initial coordinates", "[task_1]") {
     REQUIRE(alien.y_coordinate == -1);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
+
 
 TEST_CASE("Alien has correct initial health", "[task_1]") {
     Alien alien{22, 0};
@@ -69,5 +69,5 @@ TEST_CASE("Alien collision detection with other aliens", "[task_5]") {
     REQUIRE_FALSE(alien1.collision_detection(alien2));
     REQUIRE(alien1.collision_detection(alien3));
 }
-
+#if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
