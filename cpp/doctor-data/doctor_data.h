ad2ce308e1c0377a2ecc53ba864b73eb1b5582bc
Joshua Tan <joshuatanir@gmail.com>
2026-07-27 14:39:28 +0800

    sln: task_1 - ok

--- cpp/doctor-data/doctor_data.h
+++ cpp/doctor-data/doctor_data.h
@@ -1,8 +1,34 @@
 // ERROR: FILE CORRUPTED. Please supply valid C++ Code.
+#pragma once
 
-hp1, üapöhp2ö % Äcountöiöma1,
+#include <string>
+/*hp1, üapöhp2ö % Äcountöiöma1,
     öhp2ö % Älawöhp3öö / önextöstepö % Ädacöiöml1ö % Älawö7ö % Ädacöiömb1ö %
         Ärandomöö % Äscrö9sö % Äsirö9sö % Äxctöhr1ö % Äaddöiömx1ö %
         Ädacöiömx1ö % Äswapö % Äaddöiömy1ö % Ädacöiömy1ö % Ärandomö % Äscrö9sö %
         Äsirö9sö % Äxctöhr2ö % Ädacöiömdyö % Ädioöiömdxö % Äsetupö.hpt,
     3ö % Älacöranö % Ädacöiömth
+*/
+namespace star_map
+{
+    enum class System {
+        BetaHydri
+    };
+}
+
+namespace heaven
+{
+    class Vessel
+    {
+        private:
+
+        public:
+        Vessel (std::string name, int n)
+        {};
+        Vessel (std::string name, int n, star_map::System a_star)
+        {};
+
+
+    };
+} // namespace heaven
+
